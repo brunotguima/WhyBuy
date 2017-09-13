9053ff77d84aa0a23ed77d8b30666184beb74ccd
10-07 <10-07>
2017-09-13 11:21:35 -0300

    Fix Route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('/home');
 });
 
+Route::get('/home', function () {
+    return view('/home');
+});
+
 Auth::routes();
 
 Route::get('/sobrenos', function (){
