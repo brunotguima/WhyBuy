745c68920fe20b6d3d180cf95d1c7650f86eb114
07-07 <07-07>
2017-09-28 11:48:37 -0300

    Edit HTML Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,16 +12,16 @@
         <div class="col s12 m4">
           <div class="icon-block">
             <h1 class="center light-orange-text"><i class="material-icons">flash_on</i></h1>
-            <h4 class="center">Facilidade de procura</h4>
+            <h4 class="center">Facilidade e agilidade na procura.</h4>
 
-            <h5 class="light center">O app facilita a vida do usuário, trazendo diversas opções de produtos com base nos seus interesses e o aproximando da loja e do produto.</h5>
+            <h5 class="light center">O app facilita a vida do usuário, trazendo diversas opções de produtos com base nos seus interesses, sem perder horas procurando, indo sempre direto a melhor oferta!</h5>
           </div>
         </div>
 
         <div class="col s12 m4">
           <div class="icon-block">
             <h1 class="center light-orange-text"><i class="material-icons">location_on</i></h1>
-            <h4 class="center">Baseado em Localização</h4>
+            <h4 class="center">Baseado sempre em sua Localização.</h4>
 
             <h5 class="light center">O app traz as informações pertinentes com base na sua localização. Nada de ofertas à Km de distância! Tudo bem pertinho de você.</h5>
           </div>
@@ -30,9 +30,9 @@
         <div class="col s12 m4">
           <div class="icon-block">
             <h1 class="center light-orange-text"><i class="material-icons">file_download</i></h1>
-            <h4 class="center">Apenas baixar e usar!</h4>
+            <h4 class="center">Não é um site de compras online!</h4>
 
-            <h5 class="light center">Para utilizar o App é simples. Basta apenas baixar nosso aplicativo, selecionar seus interesses e usar. Sem complicações.</h5>
+            <h5 class="light center">Se você gosta de comprar em lojas físicas, e procura sempre pelo mais barato, esse é o site pra você não perder mais tempo!</h5>
           </div>
         </div>
       </div>
