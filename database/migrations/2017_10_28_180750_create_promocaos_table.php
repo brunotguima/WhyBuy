3d8e9e5492a78cfb2af7e43a12462db9be47c762
Filicio David <filicio.david@etec.sp.gov.br>
2017-10-28 16:35:12 -0200

    PRomo   _Nao funcional

---
+++ database/migrations/2017_10_28_180750_create_promocaos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePromocaosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promocaos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nomeProd');
+            $table->string('marcaProd');
+            $table->string('valorProd');
+            $table->string('categoria');
+            $table->string('codProd');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promocaos');
+    }
+}
