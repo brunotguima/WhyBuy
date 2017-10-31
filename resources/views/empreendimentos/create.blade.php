3d7d2b96170cfb0df053ebb95cf542de3fce6d53
brunotguima <bruno.guimaraes13@etec.sp.gov.br>
2017-10-31 08:33:14 -0200

    Changes Migration fix constraint foreign key

--- resources/views/empreendimentos/create.blade.php
+++ resources/views/empreendimentos/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading orange-text"><h1 class="center ">Registro de Empreendimento </h1></div>
+                <div class="panel-heading orange-text"><h1 class="center ">Registro de Empreendimento</h1></div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('empreendimentos.store') }}" enctype="multipart/form-data">
@@ -43,15 +43,10 @@
                         <div class="col-md-6">
                             <input id="telUm" type="text" class="form-control" name="telUm"required>
                         </div>
-                        <label for="ramoAtiv" class="col m4 control-label">Ramo de Atividade:</label>
-                        <div class="col m6">
-                         <select name="ramoAtiv" id="ramoAtiv" class="form-control container">
-                         @foreach ($ramoAtividade as $ramoAtiv)
-                            <option value="{{$ramoAtiv->id}}">{{$ramoAtiv->name}}</option>
-                            @endforeach
-                         </select>
+                        <label for="ramoAtiv" class="col-md-4 control-label">Ramo de Atividade:</label>
+                        <div class="col-md-6">
+                            <input id="ramoAtiv" type="text" class="form-control" name="ramoAtiv"required>
                         </div>
-                    </br>
                         <label for="razaoSocial" class="col-md-4 control-label">Razão Social:</label>
                         <div class="col-md-6">
                             <input id="razaoSocial" type="text" class="form-control" name="razaoSocial"required>
