39e374f111615820822866515067e0c2ce2837b2
Victorhugomartoni <wrvictorbr@gmail.com>
2017-10-31 00:41:43 -0200

    mascara sem validacao

--- resources/views/perfil/create.blade.php
+++ resources/views/perfil/create.blade.php
@@ -9,7 +9,9 @@
                 <h3 class="center"><span class="orange-text">COMPLETE SEU PERFIL DE USUÁRIO</span></h3>
         </div>
                 <div class="panel-body">
+
                     <form class="form-horizontal" method="POST" action="{{ route('perfil.store') }}" enctype="multipart/form-data">
+
                         {{ csrf_field() }}
 
                         <label for="name" class="col m4 control-label">Nome:</label>
@@ -29,45 +31,63 @@
 
                         <label for="sexo" class="col m4 control-label">Sexo:</label>
                         <div class="col m6">
-                         <select name="sexo" id="sexo" class="form-control container">
-                            <option value="Masculino">Masculino</option>
-                            <<option value="Feminino">Feminino</option>
-                         </select>
+                        <form action="#">
+                        <p>
+                            <input name="sexo" type="radio" id="masculino" />
+                        <label for="masculino">Masculino</label>
+                        </p>
+                        <p>
+                            <input name="sexo" type="radio" id="feminino" />
+                        <label for="feminino">Feminino</label>
+                         </p>
+                         
                         </div>
+
                     </br>
+
                         <label for="rg" class="col m4 control-label">RG:</label>
                         <div class="col m6 input-field">
-                            <input id="rg" type="text" class="form-control rg" name="rg"required>
+                            <input id="rg" type="text" class="form-control rg" name="rg"required placeholder="00.000.000-0" maxlength="12">
                         </div>
+
                         <label for="cpf" class="col m4 control-label">CPF:</label>
                         <div class="col m6 input-field">
-                            <input id="cpf" type="text" class="cpf" name="cpf"required>
+                            <input id="cpf" type="text" class="cpf" name="cpf"required placeholder="000.000.000-00" maxlength="14">
                         </div>
+
                         <label for="telUm" class="col m4 control-label">Telefone:</label>
                         <div class="col m6 input-field">
-                            <input id="telUm" type="text" class="telUm" name="telUm"required>
+                        
+                        <input type="text" class="form-control telUm" placeholder="(00) 0000-0000" maxlength="13">                 
                         </div>
+
                         <label for="cell" class="col m4 control-label">Celular:</label>
                         <div class="col m6 input-field">
-                            <input id="cell" type="text" class="cell" name="cell"required>
+                        <input type="text" class="form-control cell" placeholder="(00) 00000-0000" maxlength="14">
                         </div>
+
                         <label for="image" class="col m4 control-label">Sua foto de Perfil:</label>
                         <div class="col m6 input-field">
                         <input id="image" type="file" class="form-control image" name="image">
                         </div>
+
                         <br>
+
                         <div class="col m6 input-field">
                             <button type="submit" class="btn btn-primary orange darken-4">
                                 Cadastrar
                             </button>
                         </div>
+
                 </div>
+
                 </form>
             </div>
         </div>
     </div>
 </div>
 </div>
+
 <script>
 $(document).ready(function(){
 $('#sexo').select2();
