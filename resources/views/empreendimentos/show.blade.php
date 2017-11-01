5a61bd75c56d3f24d55dd6f8233dfffc06d2e208
07-06 <07-06>
2017-11-01 10:37:42 -0200

    show atualizado

--- resources/views/empreendimentos/show.blade.php
+++ resources/views/empreendimentos/show.blade.php
@@ -17,34 +17,61 @@
           </span>
       </div>
     </div>
-    <img src="{{asset('/images/empreendimentos')}}/{{$emp->EmpImage}}">
+    <footer class="page-footer">
+            <div class="container">
+                <img class="circle center-align" width="150" height="150" src="{{asset('/images/empreendimentos')}}/{{$emp->EmpImage}}">
+<br />
+<br />
 
-            <label for="nomeFantasia" class="control-label">Nome Fantasia:</label>
-            <h5 class="black-text"></h5>
+<div class="row center">
+                
+                <div class="row">
+                    
+                        <h4 class="center-align" class="white-text" >INFORMAÇÕES DO EMPREENDEDORISMO</h4>
+                        <li class="divider"></li>
+                        
+                        <ul>
+                            <div class="center-align">
+                            <li>  
+                             <label for="nomeFantasia" class="control-label white-text ">Nome Fantasia:</label>
+            <h7 class="white-text">{{$emp->nomeFantasia}}</h7>
+                                <br />
 
-            <label for="cnpj" class="control-label">CNPJ:</label>
-            <h5 class="black-text">{{$emp->cnpj}}</h5>
+                                <label for="cnpj" class="control-label white-text">CNPJ:</label>
+            <h7 class="white-text">{{$emp->cnpj}}</h7>
+            <br />
 
-            <label for="inscEst" class="control-label">Inscrição Estadual:</label>
-            <h5 class="black-text">{{$emp->inscEst}}</h5>
+            <label for="inscEst" class="control-label white-text">Inscrição Estadual:</label>
+            <h7 class="white-text">{{$emp->inscEst}}</h7>
+            <br />
 
-            <label for="cep" class="control-label">CEP:</label>
-            <h5 class="black-text">{{$emp->cep}}</h5>
+            <label for="cep" class="control-label white-text">CEP:</label>
+            <h7 class="white-text">{{$emp->cep}}</h7>
+            <br />
 
-            <label for="cidade" class="control-label">Cidade:</label>
-            <h5 class="black-text">{{$emp->cidade}}</h5>
+            <label for="cidade" class="control-label white-text">Cidade:</label>
+            <h7 class="white-text">{{$emp->cidade}}</h7>
+            <br />
 
-            <label for="estado" class="control-label">Estado:</label>
-            <h5 class="black-text">{{$emp->estado}}</h5>
+            <label for="estado" class="control-label white-text">Estado:</label>
+            <h7 class="white-text">{{$emp->estado}}</h7>
+            <br />
 
-            <label for="razaoSocial" class="control-label">Razão Social:</label>
-            <h5 class="black-text">{{$emp->razaoSocial}}</h5>
+            <label for="razaoSocial" class="control-label white-text">Razão Social:</label>
+            <h7 class="white-text">{{$emp->razaoSocial}}</h7>
+            <br />
 
-            <label for="ramoAtiv" class="control-label">Ramo de Atividade:</label>
-            <h5 class="black-text">{{$emp->RamoAtivEmpreendimento->name}}
+            <label for="ramoAtiv" class="control-label white-text">Ramo de Atividade:</label>
+            <h7 class="white-text">{{$emp->RamoAtivEmpreendimento->name}}
 @endforeach
+                               
+                        </ul>
+                    
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
     </div>
 </div>
-</div>
-
-@endsection
