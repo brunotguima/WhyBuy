701bcab99495161e74581f0ebdc5d594398e32d7
brunotguima <bruno.guimaraes13@etec.sp.gov.br>
2017-11-22 08:22:37 -0200

    alteraçoes html

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,18 +5,18 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading "><h1 class="center orange-text">LOGIN</h1></div>
+                <div class="panel-heading "><h1 class="center orange-textp">LOGIN</h1></div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <label for="email" class="col-md-4 control-label">Endereço de E-Mail</label>
+                        <label for="email" class="col-md-4 control-label">* Endereço de E-Mail</label>
                         <div class="col-md-6">
                             <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
                         </div>
 
-                        <label for="password" class="col-md-4 control-label">Senha</label>
+                        <label for="password" class="col-md-4 control-label">* Senha</label>
                         <div class="col-md-6">
                             <input id="password" type="password" class="form-control" name="password" required>
                         </div>        
