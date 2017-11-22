f76034dd25e416016485c8b31b1d44efcab6c860
brunotguima <bruno.guimaraes13@etec.sp.gov.br>
2017-11-22 08:19:02 -0200

    alterações html

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading "><h1 class="center">LOGIN</h1></div>
+                <div class="panel-heading "><h1 class="center orange-text">LOGIN</h1></div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading"><h1 class="center">Registro</h1></div>
+                <div class="panel-heading"><h1 class=" center"><span class="orange-text">Registro</span></h1></div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
